bb9ff40cc6184f2e591a30dff223698a754a35df
Godswill Udoh <godswilludoh@gmail.com>
2022-07-05 00:37:20 +0100

    made changes to the navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/service' element={<Service />} />
-					<Route path='/blog' element={<Blog />} />
-					<Routes path='/contact' element={<Contact />} />
-				</Routes>
-			</BrowserRouter>
+			<Navbar />
 		</div>
 	);
 }
