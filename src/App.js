45030de2f96d03708d6655b99c3a8ffd08b4c203
Godswill Udoh <godswilludoh@gmail.com>
2022-07-05 01:20:32 +0100

    worked on sectionone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import SectionOne from './components/sectionone/SectionOne';
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
+			<SectionOne />
 		</div>
 	);
 }
